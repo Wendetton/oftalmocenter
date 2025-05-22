4ac23a4214d96d19f194cfa854d30d78caa93066
Wendetton <feazegoncalves@gmail.com>
2025-05-21 22:18:02 -0300

    Update [id].js

--- pages/api/medicos/[id].js
+++ pages/api/medicos/[id].js
@@ -1,6 +1,6 @@
-import dbConnect from '../../../lib/mongodb';
-import Medico from '../../../models/Medico';
-import { protect } from '../../../lib/auth';
+import dbConnect from '../../lib/mongodb';
+import Medico from '../../models/Medico';
+import { protect } from '../../lib/auth';
 
 export default async function handler(req, res) {
   const { method } = req;
