b4d4209e37f5e200c3fa296a51c06066e302bba0
Wendetton <feazegoncalves@gmail.com>
2025-05-21 22:29:22 -0300

    Update index.js

--- pages/api/validacao/index.js
+++ pages/api/validacao/index.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../../lib/mongodb';
-import { validarCPF, validarEmail, validarTelefone } from '../../../lib/auth';
+import dbConnect from '../../lib/mongodb';
+import { validarCPF, validarEmail, validarTelefone } from '../../lib/auth';
 
 export default async function handler(req, res) {
   const { method } = req;
