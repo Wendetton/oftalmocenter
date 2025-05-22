88d2a9bd64702407b0511665ca8fe493c9b2920c
Wendetton <feazegoncalves@gmail.com>
2025-05-21 21:42:08 -0300

    Update setup.js

--- pages/api/setup.js
+++ pages/api/setup.js
@@ -1,5 +1,5 @@
-import dbConnect from '../../../lib/mongodb';
-import Usuario from '../../../models/Usuario';
+import dbConnect from '../../lib/mongodb';
+import Usuario from '../../models/Usuario';
 
 export default async function handler(req, res) {
   const { method } = req;
