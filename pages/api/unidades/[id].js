dd49e6cf1da93db33b78aac1463ae65d2552d5af
Wendetton <feazegoncalves@gmail.com>
2025-05-21 22:19:42 -0300

    Update [id].js

--- pages/api/unidades/[id].js
+++ pages/api/unidades/[id].js
@@ -1,6 +1,6 @@
-import dbConnect from '../../../lib/mongodb';
-import Unidade from '../../../models/Unidade';
-import { protect } from '../../../lib/auth';
+import dbConnect from '../../lib/mongodb';
+import Unidade from '../../models/Unidade';
+import { protect } from '../../lib/auth';
 
 export default async function handler(req, res) {
   const { method } = req;
