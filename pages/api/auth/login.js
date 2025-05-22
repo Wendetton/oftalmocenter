80eca09657706de0f0194bbb24a7eb07f8f41a34
Wendetton <feazegoncalves@gmail.com>
2025-05-21 22:40:35 -0300

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,6 +1,6 @@
-import dbConnect from '../../lib/mongodb';
-import Usuario from '../../models/Usuario';
-import { generateToken } from '../../lib/auth';
+import dbConnect from '../../../lib/mongodb';
+import Usuario from '../../../models/Usuario';
+import { generateToken } from '../../../lib/auth';
 import cookie from 'cookie';
 
 export default async function handler(req, res) {
